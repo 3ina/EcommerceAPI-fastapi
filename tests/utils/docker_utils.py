fc4b425c0c23b6e3f9b9c6a96cc950c9407117da
sinaroydel <3inaroydl@gmail.com>
2025-01-25 16:10:26 +0330

    add start_database_container function

---
+++ tests/utils/docker_utils.py
@@ -0,0 +1,28 @@
+import docker
+from docker.errors import NotFound
+
+def start_database_container():
+    client = docker.from_env()
+    container_name = "test-db"
+
+    try:
+        existing_container = client.containers.get(container_name)
+        print(f"Existing container {container_name}. Stopping and removing it.")
+        existing_container.stop()
+        existing_container.remove()
+        print(f"Container {container_name} stopped and removed.")
+    except NotFound:
+        print(f"Container {container_name} not found.")
+
+    container_config = {
+        "name":container_name,
+        "image":"postgres:16.1-alpine3.19",
+        "detach": True,
+        "ports":{"5432":"5434"},
+        "environment":{
+            "POSTGRES_USER":"postgres",
+            "POSTGRES_PASSWORD":"postgres",
+        }
+    }
+
+    container = client.containers.run(**container_config)
