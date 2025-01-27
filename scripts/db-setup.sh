c917988ea393dcc5bdc53d7fbfe96ad113ec5511
sinaroydel <3inaroydl@gmail.com>
2025-01-27 12:53:45 +0330

    fix typo

--- scripts/db-setup.sh
+++ scripts/db-setup.sh
@@ -4,4 +4,4 @@ export PGUSER="postgres"
 
 psql -c "CREATE DATABASE inventory"
 
-psql inventory -c "CREATE EXTENSIONS IF NOT EXISTS \"uuid-ossp\";"
+psql inventory -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
