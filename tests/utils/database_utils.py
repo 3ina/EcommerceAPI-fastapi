fab18b1a58601c42996e70fe671cefcf73ed1c3f
sinaroydel <3inaroydl@gmail.com>
2025-01-30 15:56:55 +0330

    update migrate_to_db func

--- tests/utils/database_utils.py
+++ tests/utils/database_utils.py
@@ -1,11 +1,45 @@
+import os
+
 import alembic.config
 from alembic import command
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def migrate_to_db(
-        script_location,alembic_ini_path="alembic.ini",
-        connection=None,revision="head"
+        script_location : str,
+        alembic_ini_path : str ="alembic.ini",
+        connection=None,
+        revision : str ="head"
         ):
-    config = alembic.config.Config(alembic_ini_path)
-    if connection is None:
-        config.config_ini_section = "testdb"
-        command.upgrade(config, revision)
+    """
+       Apply database migrations using Alembic.
+
+       Args:
+           script_location (str): Path to the Alembic script directory.
+           alembic_ini_path (str): Path to the Alembic configuration file.
+           connection: Optional database connection object. If provided, Alembic will use this connection.
+           revision (str): The revision to upgrade to (default is "head").
+       """
+
+    try:
+        alembic_cfg = alembic.config.Config(alembic_ini_path)
+        alembic_cfg.set_main_option("script_location", script_location)
+
+        if connection is not None:
+            alembic_cfg.attributes["connection"] = connection
+            logger.info("Using provided database connection for migrations.")
+        else:
+            # Use the testdb section in alembic.ini
+            TEST_DATABASE_URL = os.environ.get('TEST_DATABASE_URL')
+            alembic_cfg.set_main_option("sqlalchemy.url", TEST_DATABASE_URL)
+            logger.info("Using testdb configuration from alembic.ini.")
+
+        logger.info(f"Applying migrations to revision: {revision}")
+        command.upgrade(alembic_cfg, revision)
+        logger.info("Migrations applied successfully.")
+    except Exception as e:
+        logger.error(f"Failed to apply migrations: {e}")
+        raise
+
