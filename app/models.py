40acdcb76c37c7c5e0e4b35ba44b2f217138056f
sinaroydel <3inaroydl@gmail.com>
2025-01-31 11:53:51 +0330

    update model

--- app/models.py
+++ app/models.py
@@ -8,9 +8,9 @@ from sqlalchemy import (
 
 class Category(Base):
     __tablename__ = "category"
-    id = Column(INTEGER, primary_key=True)
-    name = Column(String(100))
-    slug = Column(String(120))
-    is_active = Column(Boolean)
-    level = Column(Integer)
-    parent_id = Column(Integer)
+    id = Column(INTEGER, primary_key=True,nullable=False)
+    name = Column(String(100), nullable=False)
+    slug = Column(String(120), nullable=False)
+    is_active = Column(Boolean, nullable=False)
+    level = Column(Integer,nullable=False)
+    parent_id = Column(Integer,nullable=True)
