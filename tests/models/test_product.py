b886f68b0ac57ab5756712746ba761edf37fc508
sinaroydel <3inaroydl@gmail.com>
2025-02-02 12:11:47 +0330

    add test_model_structure_nullable_constraints

--- tests/models/test_product.py
+++ tests/models/test_product.py
@@ -20,4 +20,30 @@ def test_model_structure_column_data_types(db_inspector):
     assert isinstance(columns["is_active"]["type"],Boolean)
     assert isinstance(columns["stock_status"]["type"],Enum)
     assert isinstance(columns["category_id"]["type"],Integer)
-    assert isinstance(columns["seasonal"]["type"],Integer)
+    assert isinstance(columns["seasonal_event"]["type"],Integer)
+
+
+def test_model_structure_nullable_constraints(db_inspector):
+    table = "product"
+    columns = db_inspector.get_columns(table)
+
+    expected_nullable  = {
+        "id":False,
+        "pid":False,
+        "name":False,
+        "slug":False,
+        "description":True,
+        "is_digital":False,
+        "created_at":False,
+        "updated_at":False,
+        "is_active":False,
+        "stock_status":False,
+        "category_id":False,
+        "seasonal_event":True,
+
+    }
+
+    for column in columns:
+        column_name = column["name"]
+        assert column["nullable"]  == expected_nullable[column_name],,f"column {column_name} is not nullable as expected"
+
