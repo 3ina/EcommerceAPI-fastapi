adde9caa3b81604b2cf8fc41885bd1db84cfe40e
sinaroydel <3inaroydl@gmail.com>
2025-02-04 17:32:19 +0330

    add test_model_structure_column_data_types for seasonal_event

--- tests/models/test_seasonal_event.py
+++ tests/models/test_seasonal_event.py
@@ -1,3 +1,15 @@
+from sqlalchemy import DateTime, Integer, String
+
+
 def test_model_structure_table_exists(db_inspector):
     assert db_inspector.has_table("seasonal_event")
 
+
+def test_model_structure_column_data_types(db_inspector):
+    table = "seasonal_event"
+    columns = {column["name"]: column for column in db_inspector.get_columns(table)}
+
+    assert isinstance(columns["id"]["type"], Integer)
+    assert isinstance(columns["start_date"]["type"], DateTime)
+    assert isinstance(columns["end_date"]["type"], DateTime)
+    assert isinstance(columns["name"]["type"], String)
