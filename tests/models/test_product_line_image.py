b766eed0207d7d5f77e9345c2d66595a3896bd02
sinaroydel <3inaroydl@gmail.com>
2025-02-04 15:39:02 +0330

    add test_model_structure_nullable_constraints

--- tests/models/test_product_line_image.py
+++ tests/models/test_product_line_image.py
@@ -22,3 +22,23 @@ def test_model_structure_column_data_types(db_inspector):
 
 
 
+
+def test_model_structure_nullable_constraints(db_inspector):
+    table = "product_image"
+    columns = db_inspector.get_columns(table)
+
+    expected_nullable = {
+        "id": False,
+        "alternative_text": False,
+        "url": False,
+        "order": False,
+        "product_line_id": False,
+    }
+
+    for column in columns:
+        column_name = column["name"]
+        assert column["nullable"] == expected_nullable.get(
+            column_name
+        ), f"column '{column_name}' is not nullable as expected"
+
+
