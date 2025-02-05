210628050c053b0421fe68662f4b28f33bfc781d
sinaroydel <3inaroydl@gmail.com>
2025-02-05 20:52:47 +0330

    add test_model_structure_nullable_constraints for attribute model

--- tests/models/test_attribute.py
+++ tests/models/test_attribute.py
@@ -13,3 +13,18 @@ def test_model_structure_column_data_types(db_inspector):
     assert isinstance(columns["name"]["type"], String)
     assert isinstance(columns["description"]["type"], String)
 
+def test_model_structure_nullable_constraints(db_inspector):
+    table = "attribute"
+
+    columns = db_inspector.get_columns(table)
+    expected_nullable = {
+        "id" : False,
+        "name" : False,
+        "description" : False,
+    }
+
+    for column in columns:
+        column_name = column["name"]
+        assert column["nullable"] == expected_nullable.get(column_name, False),f"{column_name} is not nullable"
+
+
