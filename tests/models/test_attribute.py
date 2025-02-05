d3fceb7f1b57c161932e50e8ef47a62cfa9a5ca6
sinaroydel <3inaroydl@gmail.com>
2025-02-05 20:52:16 +0330

    add test_model_structure_column_data_types for attribute model

--- tests/models/test_attribute.py
+++ tests/models/test_attribute.py
@@ -1,2 +1,15 @@
+from sqlalchemy import Integer, String
+
+
 def test_model_structure_table_exists(db_inspector):
     assert db_inspector.has_table("attribute")
+
+
+def test_model_structure_column_data_types(db_inspector):
+    table = "attribute"
+    columns = {column["name"] : column for column in db_inspector.get_columns(table)}
+
+    assert isinstance(columns["id"]["type"], Integer)
+    assert isinstance(columns["name"]["type"], String)
+    assert isinstance(columns["description"]["type"], String)
+
