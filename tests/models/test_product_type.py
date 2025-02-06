8fca02bf3276280ca97dcb823a2506ec84437b8a
sinaroydel <3inaroydl@gmail.com>
2025-02-06 11:13:22 +0330

    add test_model_structure_column_data_types for product_type model

--- tests/models/test_product_type.py
+++ tests/models/test_product_type.py
@@ -1,4 +1,14 @@
-
+from sqlalchemy import Integer,String
 
 def test_model_structure_table_exists(db_inspector):
     assert db_inspector.has_table("product_type")
+
+
+def test_model_structure_column_data_types(db_inspector):
+    table = "product_type"
+    columns = {column["name"]: column for column in db_inspector.get_columns(table)}
+
+    assert isinstance(columns["id"]["type"], Integer)
+    assert isinstance(columns["name"]["type"], String)
+    assert isinstance(columns["level"]["type"], Integer)
+    assert isinstance(columns["parent_id"]["type"], Integer)
