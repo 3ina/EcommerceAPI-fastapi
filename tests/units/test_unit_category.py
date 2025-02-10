ab47ce33c905a8cccd52b7453c12d1e997343b1a
sinaroydel <3inaroydl@gmail.com>
2025-02-10 15:37:12 +0330

    update test_unit_create_new_category_successfully

--- tests/units/test_unit_category.py
+++ tests/units/test_unit_category.py
@@ -2,6 +2,12 @@ import pytest
 from pydantic import ValidationError
 
 from app.schemas.category_schema import CategoryCreate
+from tests.factories.models_factory import get_random_category_dict
+
+
+def mock_output(return_value=None):
+    return lambda *args,**kwargs: return_value
+
 
 
 def test_unit_schema_category_validation():
@@ -20,3 +26,18 @@ def test_unit_schema_category_validation():
     }
     with pytest.raises(ValidationError):
         CategoryCreate(**invalid_data)
+
+
+
+def test_unit_create_new_category_successfully(client,monkeypatch):
+    category = get_random_category_dict()
+
+    monkeypatch.setattr("sqlalchemy.orm.Query.first",mock_output())
+    monkeypatch.setattr("sqlalchemy.orm.Session.commit",mock_output())
+    monkeypatch.setattr("sqlalchemy.orm.Session.refresh",mock_output())
+
+    body = category.copy()
+    body.pop("id")
+    response = client.post("api/v1/category",json=body)
+    assert response.status_code == 201
+    assert response.json() == category
