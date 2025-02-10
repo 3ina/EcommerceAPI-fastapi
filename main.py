7ecbdce9fea410fd6dc28a7a3a2c3b5dd27e8886
sinaroydel <3inaroydl@gmail.com>
2025-02-10 15:47:21 +0330

    add routes for category

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
 import logging
 import logging.config
+from app.routers.v1.category_routes import router as category_router
 app = FastAPI()
 
 logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(router=category_router, prefix="/v1/category",tags=["categories"])
