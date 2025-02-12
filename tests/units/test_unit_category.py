57afc402b250006bc8b08e27d58aac886430bcc8
sinaroydel <3inaroydl@gmail.com>
2025-02-12 16:13:15 +0330

    add test_unit_create_new_category_successfully test

--- tests/units/test_unit_category.py
+++ tests/units/test_unit_category.py
@@ -1,5 +1,9 @@
 import pytest
+from fastapi import HTTPException
 from pydantic import ValidationError
+from starlette import status
+
+from app.models import Category
 
 from app.schemas.category_schema import CategoryCreate
 from tests.factories.models_factory import get_random_category_dict
@@ -32,12 +36,18 @@ def test_unit_schema_category_validation():
 def test_unit_create_new_category_successfully(client,monkeypatch):
     category = get_random_category_dict()
 
+    for key,value in category.items():
+        monkeypatch.setattr(Category,key,value)
+
     monkeypatch.setattr("sqlalchemy.orm.Query.first",mock_output())
     monkeypatch.setattr("sqlalchemy.orm.Session.commit",mock_output())
     monkeypatch.setattr("sqlalchemy.orm.Session.refresh",mock_output())
 
     body = category.copy()
     body.pop("id")
-    response = client.post("api/v1/category",json=body)
+    response =  client.post("/api/v1/category/",json=body)
     assert response.status_code == 201
     assert response.json() == category
+
+
+
